6ff8b426a758e21e1056cda74dbc351a22a8ec02
adrianctwo <adrian.e.situ@gmail.com>
2019-06-19 15:51:30 -0700

    Added function that assign a random value to each gem image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,21 @@ $(document).ready(function(){
     $("#wins").text(wins);
     $("#loses").text(loses);
 
-    var gem = ["assets/images/red.png", "assets/images/purple.png", "assets/images/green.png", "assets/images/pink.png"];
+    var gems = ["assets/images/red.png", "assets/images/purple.png", "assets/images/green.png", "assets/images/pink.png"];
 
+    // Add a random value to each Gem
+    function gemValues() {
+        
+        // We going through the whole array of images in Gems to add values to them
+        for (var i = 0; i < gems.length; i++) {
+        
+            var imageGem = $("<img>");
+            imageGem.addClass();
+            imageGem.attr("src", gems[i]);
+            // Each gem has a random value between 1 - 12 (Whole numbers only)
+            imageGem.attr("crystalValue", Math.floor(Math.random() * 12) + 1);
+            // Appending the images and values to ID gem
+            $("#gem").append(imageGem);
+        }
+    }
 })
