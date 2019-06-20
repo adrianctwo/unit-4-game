8a1412047d3dbbb0cef526025417a7b0226b5b12
adrianctwo <adrian.e.situ@gmail.com>
2019-06-20 13:59:34 -0700

    Updated two function in JS for game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
         for (var i = 0; i < gems.length; i++) {
         
             var imageGem = $("<img>");
-            imageGem.addClass();
+            imageGem.addClass("gems gemImage gemButtons");
             imageGem.attr("src", gems[i]);
             // Each gem has a random value between 1 - 12 (Whole numbers only)
             imageGem.attr("crystalValue", Math.floor(Math.random() * 12) + 1);
@@ -21,4 +21,68 @@ $(document).ready(function(){
             $("#gem").append(imageGem);
         }
     }
+
+    function game() {
+
+        // Player value always starts at 0
+        var playerValue = 0;
+
+        // Generate a random whole number between 19-120
+        var targetValue = Math.floor(Math.random() * (120 - 19) + 19);
+
+        // Show the current value of the player
+        $("#currentValue").text("Current Value: " + playerValue); 
+            
+        // Show the target value
+        $("#targetValue").text("Target Value: " + targetValue);
+
+        // When player press a gem, it calls this function.
+        $(".gemButtons").on("click", function() {
+            
+            //Assigns random number to each gem after a gem is clicked
+            gemClicked = true;
+            var gemValue = ($(this).attr("crystalValue"));
+            gemValue = parseInt(gemValue);
+            //Adds the gem value that was pressed to playerValue.
+            playerValue += gemValue;
+            // We updating the website to show the current player value after a gem is clicked
+            $("#currentValue").text("Your points: " + playerValue);
+            
+            // Alert win if player value equals to target value
+            if (playerValue === targetValue) {
+            // wins counter add 1
+            wins++;
+            // Pop-up alert
+            alert("You win!");
+            // Update website to show win score
+            $(".wins-text").text("Wins: " + wins);
+            // Reset
+            $("#gem").empty();
+            // Get new gem value
+            gemValues();
+            // Start the game again
+            game();
+            }
+            
+            // If player value goes over target value
+            else if (playerValue >= targetValue) {
+            // Losses counter add 1
+            losses++;
+            // pop-up alert
+            alert("You lose!");
+            // update website to show losses score
+            $(".losses-text").text("Loses:" + losses);
+            // reset
+            $("#gem").empty();
+            // get new gem value
+            gemValues();
+            // start the game again
+            game();
+            }
+            
+        });
+    }
+    // calling for both functions
+    gemValues();
+    game();
 })
